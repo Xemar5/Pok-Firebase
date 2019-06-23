c00e72cfa7358f586067f302caaece7ec482bc8c
sir.woody <sir.woody@theknightsofunity.com>
2019-06-23 22:24:42 +0200

    Initial Commit.

---
+++ functions/src/index.ts
@@ -0,0 +1,95 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { Timestamp } from '@google-cloud/firestore';
+admin.initializeApp();
+
+
+interface LeaderboardEntry {
+    guid: string;
+    username: string;
+    score: number;
+    invscore: number;
+    rank: number;
+}
+
+const maxLeaderboardSize = 5;
+
+
+export const getUniqueGuid = functions.https.onCall((data, context) =>
+{
+    const timestamp = Timestamp.now();
+    const ref = admin.database().ref("server/users");
+    const uid = ref.push({"registered": timestamp}).key;
+    return uid;
+});
+
+export const setScore = functions.https.onCall(async (data, context) =>
+{
+    const query = JSON.parse(data) as LeaderboardEntry;
+    console.log(query);
+    
+    const ref = admin.database().ref("server/scores");
+    await ref.child(query.guid).set(query);
+    await ref.orderByChild("invscore").once("value", async (snapshot) => 
+    {
+        let index = 1;
+        const promises: Promise<void>[] = [];
+        snapshot.forEach(x =>
+        {
+            const value = x.val();
+            value.rank = index;
+            promises.push(x.ref.update(value));
+            index += 1;
+        });
+        await Promise.all(promises);
+    });
+    return 0;
+});
+
+export const getRanking = functions.https.onCall(async (data, context) =>
+{
+    const query = JSON.parse(data) as LeaderboardEntry;
+    console.log(query);
+    
+    const entries: LeaderboardEntry[] = [];
+    let userIndex = -1;
+    const ref = admin.database().ref("server/scores");
+    await ref.orderByChild("rank").limitToFirst(maxLeaderboardSize).once("value", function(snapshot) {
+        snapshot.forEach(x => {
+            const val = x.val();
+            console.log(val);
+            
+            if (val.guid === query.guid)
+            {
+                userIndex = val.rank - 1;
+            }
+            if (val.guid !== null)
+            {
+                entries.push(val as LeaderboardEntry);
+            }
+        });
+    });
+
+    
+    if (userIndex === -1)
+    {
+        console.log("User not present. Adding to list");
+        await ref.child(query.guid).once("value", function(snapshot)
+        {
+            const local: LeaderboardEntry = snapshot.val();
+            entries.push(local);
+            userIndex = maxLeaderboardSize;
+        });
+        
+    }
+
+    const response = {
+        senderIndex: userIndex,
+        entries: entries
+    };
+
+    return JSON.stringify(response);
+});
+
+
+
